e76e6713ac9c5d62f6a8bcd754688a0ba873f1ed
vadim.r <roshchinvadim2003@gmail.com>
2024-10-18 23:28:27 +0500

    ui components: buttons and toggles

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isActiveBlueToggle: boolean = false;
+  isActiveGradientToggle: boolean = false;
 
+  toggleBlue() {
+    this.isActiveBlueToggle = !this.isActiveBlueToggle;
+  }
+
+  toggleGradient() {
+    this.isActiveGradientToggle = !this.isActiveGradientToggle;
+  }
 }
